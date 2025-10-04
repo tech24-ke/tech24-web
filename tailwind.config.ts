18f8c3cded8a44867106d3404f33d9afbbde606a
terrymuleja <thierrynsaka@gmail.com>
2025-10-04 14:21:35 +0200

    tw fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
+import type { Config } from "tailwindcss";
+
 export default {
-  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",     // catch everything under src
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: { extend: {} },
   plugins: [],
-};
+} satisfies Config;
